ab3d27df29508f76f57de7580f9a87f7bdc1ddc4
Gabrielhlc <gabrielhenlc@gmail.com>
2023-06-13 10:20:09 -0300

    add: widget locationInput

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:earth_places/providers/earth_places.dart';
 import 'package:earth_places/widgets/image_input.dart';
+import 'package:earth_places/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -57,6 +58,8 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
                     ),
                     const SizedBox(height: 10),
                     ImageInput(this._selectImage),
+                    const SizedBox(height: 10),
+                    const LocationInput(),
                   ],
                 ),
               ),
